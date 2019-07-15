138b1a304fc8a5dc8a9bcd33295ec16cec51c217
yoyo <296550494@qq.com>
2019-07-15 20:21:13 +0800

    较为稳定的版本

--- app/src/main/java/com/test/viewpagedemo/Views/TestCoordinatorLayout/dialog/DialogHeadBehavior.java
+++ app/src/main/java/com/test/viewpagedemo/Views/TestCoordinatorLayout/dialog/DialogHeadBehavior.java
@@ -1,5 +1,6 @@
 package com.test.viewpagedemo.Views.TestCoordinatorLayout.dialog;
 
+import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.support.annotation.NonNull;
@@ -21,9 +22,38 @@ public class DialogHeadBehavior extends CoordinatorLayout.Behavior {
 
     float removeY;
     boolean flg = false;
-    boolean isAnima, isUp;
+
+    boolean isAnima;
+    float downX, downY;
+    static final int MIN_MOVE = 5;
+//    boolean isUp;
+//    boolean isDown;
 
     ValueAnimator valueAnimator;
+    Animator.AnimatorListener animatorListener = new Animator.AnimatorListener() {
+        @Override
+        public void onAnimationStart(Animator animation) {
+            isAnima = true;
+        }
+
+        @Override
+        public void onAnimationEnd(Animator animation) {
+            isAnima = false;
+//            isDown = isUp = false;
+        }
+
+        @Override
+        public void onAnimationCancel(Animator animation) {
+
+            isAnima = false;
+//            isDown = isUp = false;
+        }
+
+        @Override
+        public void onAnimationRepeat(Animator animation) {
+
+        }
+    };
 
     public DialogHeadBehavior(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
@@ -43,42 +73,47 @@ public class DialogHeadBehavior extends CoordinatorLayout.Behavior {
 
 
     public boolean onInterceptTouchEvent(CoordinatorLayout parent, final View child, MotionEvent ev) {
-        LoggerUtils.LOGV("DialogHeadBehavior ev.getAction() = " + ev.getAction() + "，removeY = " + removeY + ",isUp = " + isUp);
-        if (ev.getAction() == MotionEvent.ACTION_UP) {
-            if (-removeY > 20 && isUp) {
-                //向上滑动了removeY，滑动到child.getHeight()
-                isAnima = true;
-                valueAnimator = ValueAnimator.ofFloat(-removeY, child.getHeight());
-                valueAnimator.setDuration(50);
-                valueAnimator.setInterpolator(new LinearInterpolator());
-                valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                    @Override
-                    public void onAnimationUpdate(@NonNull ValueAnimator animation) {
-                        float removeY = -(float) animation.getAnimatedValue();
-                        LoggerUtils.LOGV("DialogHeadBehavior removeY = " + removeY);
-                        child.setTranslationY(removeY);
-                    }
-                });
-                valueAnimator.start();
-                EventBus.getDefault().post(new Page2Visibily());
-            } else if (child.getHeight() + removeY > 20 && !isUp) {
-                isAnima = true;
-                valueAnimator = ValueAnimator.ofFloat(removeY, 0);
-                valueAnimator.setDuration(50);
-                valueAnimator.setInterpolator(new LinearInterpolator());
-                valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                    @Override
-                    public void onAnimationUpdate(@NonNull ValueAnimator animation) {
-                        float removeY = -(float) animation.getAnimatedValue();
-                        LoggerUtils.LOGV("DialogHeadBehavior removeY = " + removeY);
-                        child.setTranslationY(removeY);
-                    }
-                });
-                valueAnimator.start();
+        LoggerUtils.LOGV("DialogHeadBehavior ev.getAction() = " + ev.getAction() + "，removeY = " + removeY
+                + ",isAnima = " + isAnima + ",child.getHeight() = " + child.getHeight() + ",ev.getActionMasked() = " + ev.getActionMasked());
+
+        if (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_CANCEL) {
+            if (!isAnima) {
+                if (-removeY > MIN_MOVE && downY > ev.getY()) {
+                    //向上滑动了removeY，滑动到child.getHeight()
+                    valueAnimator = ValueAnimator.ofFloat(-removeY, child.getHeight());
+                    valueAnimator.setDuration(50);
+                    valueAnimator.setInterpolator(new LinearInterpolator());
+                    valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(@NonNull ValueAnimator animation) {
+                            removeY = -(float) animation.getAnimatedValue();
+                            LoggerUtils.LOGV("DialogHeadBehavior removeY = " + removeY);
+                            child.setTranslationY(removeY);
+                        }
+                    });
+                    valueAnimator.addListener(animatorListener);
+                    valueAnimator.start();
+                } else if (child.getHeight() + removeY > MIN_MOVE && downY < ev.getY()) {
+
+                    valueAnimator = ValueAnimator.ofFloat(removeY, 0);
+                    valueAnimator.setDuration(50);
+                    valueAnimator.setInterpolator(new LinearInterpolator());
+                    valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(@NonNull ValueAnimator animation) {
+                            removeY = -(float) animation.getAnimatedValue();
+                            LoggerUtils.LOGV("DialogHeadBehavior removeY = " + removeY);
+                            child.setTranslationY(removeY);
+                        }
+                    });
+                    valueAnimator.addListener(animatorListener);
+                    valueAnimator.start();
+                }
             }
         } else if (ev.getAction() == MotionEvent.ACTION_DOWN) {
-            LoggerUtils.LOGV("DialogHeadBehavior removeY = " + removeY);
-            isAnima = false;
+            downY = ev.getY();
+        } else if (ev.getAction() == MotionEvent.ACTION_MOVE) {
+
         }
         return false;
     }
@@ -96,10 +131,10 @@ public class DialogHeadBehavior extends CoordinatorLayout.Behavior {
             return;
         }
 
+
         if (removeY - dy <= 0 //向下移动的最大距离不能大于0，否则heard不能在顶部
                 && -removeY + dy < child.getHeight()/**向上移动的最大距离不能大于header的大小**/) {
             removeY -= dy;
-            isUp = dy >= 0;
             LoggerUtils.LOGD("DialogHeadBehavior offsetY child = " + child.getHeight() + ",removeY = " + removeY + ",dy = " + dy);
 
             //heard能向下滑动的最大距离
@@ -113,6 +148,7 @@ public class DialogHeadBehavior extends CoordinatorLayout.Behavior {
             //translation的最大可移动距离必须和layout的距离一致，否则会出现显示不完全
             child.setTranslationY(removeY);
             consumed[1] = dy;
+
 
         }
 
